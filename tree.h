64d284806caa2e4a5e09b56a13bba8c826c291f5
pao214 <bvsaikiranpatnaik@gmail.com>
2018-03-04 03:21:14 +0530

    Added basic classes to support basic test case.

--- tree.h
+++ tree.h
@@ -9,6 +9,9 @@ class ast_program;
 class ast_external_declaration;
 class ast_function_definition;
 class ast_type_specifier;
+class ast_identifier;
+class ast_jump_statement;
+class ast_i_constant;
 
 class ast_struct {
 
@@ -30,7 +33,31 @@ class ast_function_definition : public ast_external_declaration {
 };
 
 class ast_type_specifier {
+private:
+    Symbol type_specifier;
+public:
+    ast_type_specifier(Symbol type_specifier);
+};
 
+class ast_identifier : public ast_struct {
+private:
+    Symbol identifier;
+public:
+    ast_identifier(Symbol identifier);
+};
+
+class ast_jump_statement : public ast_struct {
+private:
+    ast_i_constant* i_constant;
+public:
+    ast_jump_statement(ast_i_constant* i_constant);
+};
+
+class ast_i_constant : public ast_struct {
+private:
+    Symbol i_constant;
+public:
+    ast_i_constant(Symbol i_constant);
 };
 
 #endif
