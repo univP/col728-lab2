6dd9f4c71763c6ae6b1250927c49df3b232a7445
Sourav Das <cs1140258@iitd.ac.in>
2018-03-22 16:48:46 +0530

    Added test case for printf

--- test/test-io.c
+++ test/test-io.c
@@ -2,5 +2,6 @@ int printf(char* format, ...);
 
 int main() {
     printf("Hello World\n");
+    printf("Int: %d, Float %f, Char %c, String %s\n", 314, 3.14, '3', "3.14");
     return 0;
 }
--- test/test.c
+++ test/test.c
@@ -1,7 +1,7 @@
 int printf(char* format, ...);
+int printf(char* format, ...);
 
 int main() {
-    printf("Hello World\n");
     printf("Int: %d, Float %f, Char %c, String %s\n", 314, 3.14, '3', "3.14");
     return 0;
 }
